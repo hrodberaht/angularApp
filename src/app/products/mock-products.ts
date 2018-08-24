402b22ef173fac976192691e05500535ea259524
Robert <seedraev@gmail.com>
2018-08-24 22:45:23 +0200

    add component product item and pass data to it

--- src/app/products/mock-products.ts
+++ src/app/products/mock-products.ts
@@ -7,5 +7,12 @@ export const PRODUCT: Array<Product> = [
     imgUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Commodore_PC20.jpg/280px-Commodore_PC20.jpg",
     price: 1200
+  },
+  {
+    name: "XBOX",
+    ean: 34234234,
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSjIOqGisSM2aHl6taxDV-NP6LXJAVHI_ln1TyuFehlIdc2Fpt",
+    price: 1500
   }
 ];
