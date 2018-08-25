415ebd2fa3b3c206c6e7fe0448a24af9cbb59b65
Robert <seedraev@gmail.com>
2018-08-25 16:41:51 +0200

    add simple routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list/product-list.component";
@@ -8,6 +9,12 @@ import { HeaderComponent } from "./header/header.component";
 import { ProductsComponent } from "./products/products.component";
 import { ProductDetailComponent } from "./products/product-detail/product-detail.component";
 import { ProductItemComponent } from "./products/product-list/product-item/product-item.component";
+import { HomeComponent } from "./home/home.component";
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "products", component: ProductsComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,9 +23,10 @@ import { ProductItemComponent } from "./products/product-list/product-item/produ
     HeaderComponent,
     ProductsComponent,
     ProductDetailComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    HomeComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
