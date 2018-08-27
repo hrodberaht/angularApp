af9d54f2efe9376bb213e365343d48db52b2171c
Robert <seedraev@gmail.com>
2018-08-27 11:18:53 +0200

    reset form add styles

--- src/app/products/add-product/add-product.component.ts
+++ src/app/products/add-product/add-product.component.ts
@@ -21,11 +21,12 @@ export class AddProductComponent implements OnInit {
   addProduct(form: NgForm) {
     this.productDataService.addProduct({
       name: form.value.name,
-      ean: +form.value.ean,
+      ean: form.value.ean,
       price: 12.3,
       imgUrl:
         "https://psmedia.playstation.com/is/image/psmedia/500mil-ds4-product-grid-01-ps4-en-15aug18_1534410394092?$ThumbnailTabs_Image$"
     });
-    this.router.navigate(["/products", "detail", form.value.ean]);
+    //this.router.navigate(["/products", "detail", form.value.ean]);
+    form.reset();
   }
 }
